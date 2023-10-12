8db4b65752885ceab1a7205ed43fef79e9b54e3c
Colter Snyder <csnyder1@mines.edu>
2023-10-12 14:18:39 -0600

    Added basic nmap

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import nmap
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def init(subnet: str = '10.0.0.0'):
+    nm = nmap.PortScanner()
+    nm.scan(subnet + '/24', arguments='-O', sudo=True)
+    return nm
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    nm = init()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for h in nm.all_hosts():
+        if 'mac' in nm[h]['addresses']:
+            print(nm[h]['addresses'], nm[h]['vendor'])
