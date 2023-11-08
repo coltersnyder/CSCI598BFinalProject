9d845a5a5787797d20ba086efbb659413ad6183f
Colter Snyder <programmer114@gmail.com>
2023-11-08 10:36:30 -0700

    Added Neo4J and started ARP Spoofin
    Added Neo4J functionality including the basic graph, and brought the proper libraries to start ARP spoofing including setting up the sockets for ARP spoofing.

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import nmap
+from neo4j import GraphDatabase
 
+# For ARP Spoofing
+# https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_arp_spoofing.htm
+import socket
+import struct
+import binascii
 
-def init(subnet: str = '10.0.0.0'):
-    nm = nmap.PortScanner()
-    nm.scan(subnet + '/24', arguments='-O', sudo=True)
-    return nm
+class IoTScanner:
+    def __init__(self, uri, user, password, eth: str = 'eth0', subnet: str = '10.0.0.0'):
+        self.nm = nmap.PortScanner()
+        self.nm.scan(subnet + '/24', arguments='-O', sudo=True)
+
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.htons(0x0800))
+        self.sock.bind((eth, socket.htons(0x0800)))
+
+    def close(self):
+        self.driver.close()
+
+    def init_graph(self):
+        with self.driver.session() as session:
+            for h in self.nm.all_hosts():
+                if 'mac' in iots.nm[h]['addresses']:
+                    device = session.execute_write(self._create_and_return_device, self.nm[h]['addresses']['mac'], self.nm[h]['vendor'])
+
+    @staticmethod
+    def _create_and_return_device(tx, mac, vendor):
+        result = tx.run("CREATE (a:Device) "
+                        "SET a.mac = $mac "
+                        "SET a.vendor = $vendor "
+                        "RETURN a.mac + ' is from vendor ' + a.vendor", mac=mac, vendor=vendor)
+        return result.single()[0]
 
 
 if __name__ == '__main__':
-    nm = init()
-
-    for h in nm.all_hosts():
-        if 'mac' in nm[h]['addresses']:
-            print(nm[h]['addresses'], nm[h]['vendor'])
+    iots = IoTScanner("bold://localhost:7687", "neo4j", "password")
+    iots.init_graph()
+    iots.close()
