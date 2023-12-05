bf5bfca120d6401531236925963a01955622bc0f
Colter Snyder <programmer114@gmail.com>
2023-12-04 21:12:27 -0700

    Cleanup Main.py
    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import csv
-import datetime
 import math
 
-import nmap
 from neo4j import GraphDatabase, RoutingControl
 
-# For ARP Spoofing
-# https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_arp_spoofing.htm
-import socket
-import struct
-import binascii
 
 class PaperScanner:
     def __init__(self, uri, user, password, eth: str = 'eth0', subnet: str = '10.0.0.0'):
@@ -17,7 +10,6 @@ class PaperScanner:
 
         self.data = []
 
-        '''
         with open("data/UNSW_2018_IoT_Botnet_Full5pc_1.csv") as csvfile:
             csvreader = csv.DictReader(csvfile)
             for row in csvreader:
@@ -41,7 +33,6 @@ class PaperScanner:
                     stime=row['stime'],
                     database_='neo4j'
                 )
-        '''
 
         print()
         print("===============")
@@ -233,38 +224,5 @@ class PaperScanner:
                 print(f'{node["a.addr"]} had {rel["count"]} attempts to connect')
 
 
-class IoTScanner:
-    def __init__(self, uri, user, password, eth: str = 'eth0', subnet: str = '10.0.0.0'):
-        self.nm = nmap.PortScanner()
-        self.nm.scan(subnet + '/24', arguments='-O', sudo=True)
-
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.htons(0x0800))
-        self.sock.bind((eth, socket.htons(0x0800)))
-
-    def close(self):
-        self.driver.close()
-
-    def init_graph(self):
-        with self.driver.session() as session:
-            for h in self.nm.all_hosts():
-                if 'mac' in iots.nm[h]['addresses']:
-                    device = session.execute_write(self._create_and_return_device, self.nm[h]['addresses']['mac'], self.nm[h]['vendor'])
-
-    @staticmethod
-    def _create_and_return_device(tx, mac, vendor):
-        result = tx.run("CREATE (a:Device) "
-                        "SET a.mac = $mac "
-                        "SET a.vendor = $vendor "
-                        "RETURN a.mac + ' is from vendor ' + a.vendor", mac=mac, vendor=vendor)
-        return result.single()[0]
-
-
 if __name__ == '__main__':
-    '''
-    iots = IoTScanner("bolt://localhost:7687", "neo4j", "password")
-    iots.init_graph()
-    iots.close()
-    '''
     ps = PaperScanner("bolt://localhost:7687", "neo4j", "test1234")
